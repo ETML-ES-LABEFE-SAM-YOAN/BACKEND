13f26601bf6a351130f8cecf20e7eb8afa6c4f2e
Sam <sam.dreneau@eduvaud.ch>
2025-05-26 09:23:42 +0200

    REFACTOR : Supprimer toutes traces de l'ancien projet et renommer avec le nom du projet

--- src/main/java/ch/etmles/bidster/Exemple/Repositories/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.etmles.bidster.Exemple.Repositories;
-
-import ch.etmles.bidster.Exemple.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
